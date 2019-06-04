a9ceb5569cc16b2a674dad429c4d8ac4d9673bda
Sheykoslav <mazurec777@gmail.com>
2019-06-04 23:59:30 +0300

    first commit

---
+++ underscore.js
@@ -0,0 +1,9 @@
+var arrOfValues = _.range(-50, 30, 4);
+var shuffledArray = _.shuffle(arrOfValues);
+var maxValue = _.max(shuffledArray);
+var minValue = _.min(shuffledArray);
+var maxIndex = _.findIndex(shuffledArray, function(value){return value == maxValue});
+var minIndex = _.findIndex(shuffledArray, function(value){return value == minValue});
+console.log(shuffledArray);
+console.log(maxIndex);
+console.log(minIndex);
